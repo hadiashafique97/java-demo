bd4719203dd31d1077b6a3db2618b5c3838873a4
hadia <hadiashafique@icloud.com>
2023-11-22 11:51:57 -0600

    java-console-rock-peper-scissors

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,46 @@
+import java.util.Objects;
+import java.util.Random;
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+//        every java program ahs one main method and thats where the program starts
+        System.out.println("Do you want to choose rock, paper, or scissors?");
+        Scanner sc = new Scanner(System.in);
+        String choice = sc.nextLine();
+        System.out.println(choice);
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        // randomly generate the computer to choose between rock paper and scissors
+        String [] computerChoice = {"rock", "paper", "scissors"};
+        Random random = new Random();
+        int randomIndex = random.nextInt(computerChoice.length);
+        String randomString = computerChoice[randomIndex];
+        System.out.println(randomString);
+
+        if(randomString.equals("rock") && choice.equals( "paper")) {
+            System.out.println("User wins");
         }
+        else if(randomString.equals("paper") && choice.equals("scissors")) {
+            System.out.println("User wins");
+        }
+        else if(randomString.equals("scissors") && choice.equals("rock")) {
+            System.out.println("User wins");
+        }
+        else if(randomString.equals("scissors") && choice.equals("paper")){
+            System.out.println("computer Wins");
+        }
+        else if(randomString.equals("paper") && choice.equals("rock")){
+            System.out.println("computer Wins");
+        }
+        else if(randomString.equals("rock") && choice.equals("scissors")) {
+            System.out.println("computer wins");
+        }
+        else {
+            System.out.println("ITS A TIE, Try Again :/");
+        }
+
     }
 }
