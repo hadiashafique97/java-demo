08fc56601283f80a067eb6fa665d318c28ae3811
hadia <hadiashafique@icloud.com>
2023-11-22 12:04:07 -0600

    java-console-rock-paper-scissors

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -7,40 +6,47 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 //        every java program ahs one main method and thats where the program starts
-        System.out.println("Do you want to choose rock, paper, or scissors?");
-        Scanner sc = new Scanner(System.in);
-        String choice = sc.nextLine();
-        System.out.println(choice);
+        boolean gameOver = false;
+        while (!gameOver) {
+            System.out.println("Do you want to choose rock, paper, or scissors?");
+            Scanner sc = new Scanner(System.in);
+            String choice = sc.nextLine();
+            System.out.println(choice);
 
+            String[] computerChoice = {"rock", "paper", "scissors"};
+            Random random = new Random();
+            int randomIndex = random.nextInt(computerChoice.length);
+            String randomString = computerChoice[randomIndex];
+            System.out.println(randomString);
+            // randomly generate the computer to choose between rock paper and scissors
+            if (randomString.equals("rock") && choice.equals("paper")) {
+                System.out.println("User wins");
+                gameOver = true;
+            } else if (randomString.equals("paper") && choice.equals("scissors")) {
+                System.out.println("User wins");
+                gameOver = true;
+            } else if (randomString.equals("scissors") && choice.equals("rock")) {
+                System.out.println("User wins");
+                gameOver = true;
+            } else if (randomString.equals("scissors") && choice.equals("paper")) {
+                System.out.println("computer Wins");
+                gameOver = true;
+            } else if (randomString.equals("paper") && choice.equals("rock")) {
+                System.out.println("computer Wins");
+                gameOver = true;
+            } else if (randomString.equals("rock") && choice.equals("scissors")) {
+                System.out.println("computer wins");
+                gameOver = true;
+            } else {
+                System.out.println("ITS A TIE, Try Again :/");
+            }
 
-        // randomly generate the computer to choose between rock paper and scissors
-        String [] computerChoice = {"rock", "paper", "scissors"};
-        Random random = new Random();
-        int randomIndex = random.nextInt(computerChoice.length);
-        String randomString = computerChoice[randomIndex];
-        System.out.println(randomString);
-
-        if(randomString.equals("rock") && choice.equals( "paper")) {
-            System.out.println("User wins");
-        }
-        else if(randomString.equals("paper") && choice.equals("scissors")) {
-            System.out.println("User wins");
-        }
-        else if(randomString.equals("scissors") && choice.equals("rock")) {
-            System.out.println("User wins");
-        }
-        else if(randomString.equals("scissors") && choice.equals("paper")){
-            System.out.println("computer Wins");
-        }
-        else if(randomString.equals("paper") && choice.equals("rock")){
-            System.out.println("computer Wins");
-        }
-        else if(randomString.equals("rock") && choice.equals("scissors")) {
-            System.out.println("computer wins");
-        }
-        else {
-            System.out.println("ITS A TIE, Try Again :/");
         }
 
     }
+
 }
+
+
+
+
